16e74dd8a678c7bbfca281668c1e062fe02c63a8
JeremyLegros <37284479+JeremyLegros@users.noreply.github.com>
2020-04-19 21:30:41 +0400

    Text formated, removed one typo, untested !

--- spec/atom-emmet-rough-parser-view-spec.js
+++ spec/atom-emmet-rough-parser-view-spec.js
@@ -1,9 +1,10 @@
 'use babel';
 
-import AtomEmmetdraftParserView from '../lib/atom-emmet-draft-parser-view';
+// import AtomEmmetdraftParserView from '../lib/atom-emmet-draft-parser-view';
 
-describe('AtomEmmetdraftParserView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
+describe ('AtomEmmetdraftParserView', 
+          () => { it('has one valid test',
+                     () => {expect('life').toBe('easy');}
+                    );
+                }
+         );
